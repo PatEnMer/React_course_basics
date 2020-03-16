05ef3963874f65eaed8f3492b8a8aa9eb10a54df
PatEnMer <patryk.nowak@gmail.com>
2020-03-16 10:11:33 +0100

    preparation of the app structure

---
+++ 01/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => (
+	<div className="wrapper">
+		<h1 className="mainHeader">Hello, hello</h1>
+		<h2 className="secondaryHeader">Heja, hej</h2>
+	</div>
+);
+
+export default App;
--- 01/src/index.js
+++ 01/src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const App = () => (
-	<div className="wrapper">
-		<h1 className="mainHeader">Hello, hello</h1>
-		<h2 className="secondaryHeader">Heja, hej</h2>
-	</div>
-);
-
-ReactDOM.render(
-	//rendered element
-	<App />,
-	// <h1>Siema, ściema</h1>,
-	// target
-	document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
