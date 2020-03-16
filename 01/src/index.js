d5a176fb101ec23916acbb5c7af7074b99e460a0
PatEnMer <patryk.nowak@gmail.com>
2020-03-16 09:33:55 +0100

    how JSX works

--- 01/src/index.js
+++ 01/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = React.createElement('h1', null, 'Heja, heja, hej');
+const App = React.createElement('div', { className: 'wrapper', items: '2' }, [
+	React.createElement('h1', { className: 'mainHeader' }, 'Hello, hello'),
+	React.createElement('h2', { className: 'secondaryHeader' }, 'Siema, siema')
+]);
 
 ReactDOM.render(
 	//rendered element
