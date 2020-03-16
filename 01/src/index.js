854a19875aba47c5bd695e9d303f1023370a528b
PatEnMer <patryk.nowak@gmail.com>
2020-03-16 09:24:27 +0100

    how work jsx - adding content

--- 01/src/index.js
+++ 01/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = React.createElement('h1', null, 'Heja, heja, hej');
+
 ReactDOM.render(
 	//rendered element
-	<h1>Siema, ściema</h1>,
+	App,
+	// <h1>Siema, ściema</h1>,
 	// target
 	document.getElementById('root')
 );
