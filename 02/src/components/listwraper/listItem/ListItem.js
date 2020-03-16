fb11090e5176b57d1d926b324b3d6b0f439af7f5
PatEnMer <patryk.nowak@gmail.com>
2020-03-16 13:19:01 +0100

    styling component ListItem

--- 02/src/components/listwraper/listItem/ListItem.js
+++ 02/src/components/listwraper/listItem/ListItem.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import './ListItem.css';
+import ryanFlorenceImage from '../../../assets/images/ryanflorence.jpg';
 
-const ListItem = () => <li className="listItemWrapper"> item1 </li>;
+const ListItem = () => (
+	<li className="listItem__wrapper">
+		<img src={ryanFlorenceImage} className="listItem__image" />
+		<div>
+			<h2 className="listItem__name">Ryan Florence</h2>
+			<p className="listItem__description">
+				Making React accessible for users and developers at https://reach.tech . Online learning, workshops,
+				OSS, and consulting.
+			</p>
+			<button className="listItem__button">visit twitter page</button>
+		</div>
+	</li>
+);
 
 export default ListItem;
