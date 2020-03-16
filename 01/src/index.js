229df5cf894023fe295a344f00a40f69b0185256
PatEnMer <patryk.nowak@gmail.com>
2020-03-16 09:58:23 +0100

    creating a functional component

--- 01/src/index.js
+++ 01/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = React.createElement('div', { className: 'wrapper', items: '2' }, [
-	React.createElement('h1', { className: 'mainHeader' }, 'Hello, hello'),
-	React.createElement('h2', { className: 'secondaryHeader' }, 'Siema, siema')
-]);
+const App = () => (
+	<div className="wrapper">
+		<h1 className="mainHeader">Hello, hello</h1>
+		<h2 className="secondaryHeader">Heja, hej</h2>
+	</div>
+);
 
 ReactDOM.render(
 	//rendered element
-	App,
+	<App />,
 	// <h1>Siema, ściema</h1>,
 	// target
 	document.getElementById('root')
