15dd1d9009de45b00ccdac584211d4c66f5bc219
PatEnMer <patryk.nowak@gmail.com>
2020-03-17 15:17:36 +0100

    adding props

--- 02/src/App.js
+++ 02/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ListWraper from './components/listwraper/listwraper';
 import './index.css';
+import { twitterAccounts } from './data/twitterAccounts';
 
 const App = () => (
 	<div>
-		<ListWraper />
+		<ListWraper name={twitterAccounts[0].name} />
 	</div>
 );
 
--- 02/src/components/listwraper/listItem/ListItem.js
+++ 02/src/components/listwraper/listItem/ListItem.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './ListItem.css';
-import ryanFlorenceImage from '../../../assets/images/ryanflorence.jpg';
 
-const ListItem = () => (
+const ListItem = (props) => (
 	<li className="listItem__wrapper">
-		<img src={ryanFlorenceImage} className="listItem__image" />
+		<img src={props.image} className="listItem__image" />
 		<div>
-			<h2 className="listItem__name">Ryan Florence</h2>
-			<p className="listItem__description">
-				Making React accessible for users and developers at https://reach.tech . Online learning, workshops,
-				OSS, and consulting.
-			</p>
+			<h2 className="listItem__name">{props.name}</h2>
+			<p className="listItem__description">{props.description}</p>
 			<button className="listItem__button">visit twitter page</button>
 		</div>
 	</li>
