da30cb0b752dd0a8eb7b1c1ef649d82ff54caa4f
PatEnMer <patryk.nowak@gmail.com>
2020-03-17 18:19:42 +0100

    modify state component

--- 03/src/components/ListWrapper/MyComponent/MyComponent.js
+++ 03/src/components/ListWrapper/MyComponent/MyComponent.js
@@ -2,12 +2,21 @@ import React from "react";
 
 class MyComponent extends React.Component {
   state = {
-    text: "test state"
+    text: ""
   };
+
+  handleChange = e => {
+    this.setState({ text: e.target.value.toUpperCase() });
+  };
+
   render() {
     return (
       <>
-        <input placeholder="your text" onChange={this.handleChange} />
+        <input
+          placeholder="your text"
+          onChange={this.handleChange}
+          value={this.state.text}
+        />
         <h1>{this.state.text}</h1>
       </>
     );
