a2bd5ac3c8e6170bd4e2d3a462ac3d99f0a91b00
PatEnMer <patryk.nowak@gmail.com>
2020-03-17 15:53:39 +0100

    array map

--- 02/src/components/listwraper/listItem/ListItem.js
+++ 02/src/components/listwraper/listItem/ListItem.js
@@ -3,11 +3,13 @@ import './ListItem.css';
 
 const ListItem = (props) => (
 	<li className="listItem__wrapper">
-		<img src={props.image} className="listItem__image" />
+		<img src={props.image} className="listItem__image" alt={props.name} />
 		<div>
 			<h2 className="listItem__name">{props.name}</h2>
 			<p className="listItem__description">{props.description}</p>
-			<button className="listItem__button">visit twitter page</button>
+			<a href={props.link} className="listItem__button">
+				visit twitter page
+			</a>
 		</div>
 	</li>
 );
