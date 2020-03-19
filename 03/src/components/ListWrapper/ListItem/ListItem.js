437f952b14264e2b6453972221ff0b117f13eb71
PatEnMer <patryk.nowak@gmail.com>
2020-03-19 17:11:50 +0100

    css modules practise

--- 03/src/components/ListWrapper/ListItem/ListItem.js
+++ 03/src/components/ListWrapper/ListItem/ListItem.js
@@ -1,14 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./ListItem.css";
+import styles from "./ListItem.module.scss";
 
 const ListItem = ({ image, name, description, twitterLink }) => (
-  <li className="listItem__wrapper">
-    <img src={image} className="listItem__image" alt={name} />
+  <li className={styles.wrapper}>
+    <img src={image} className={styles.image} alt={name} />
     <div>
-      <h2 className="listItem__name">{name}</h2>
-      <p className="listItem__description">{description}</p>
-      <a href={twitterLink} target="_blank" className="listItem__button">
+      <h2 className={styles.name}>{name}</h2>
+      <p className={styles.description}>{description}</p>
+      <a
+        href={twitterLink}
+        target="_blank"
+        className={styles.button}
+        rel="noopener noreferrer"
+      >
         visit twitter page
       </a>
     </div>
